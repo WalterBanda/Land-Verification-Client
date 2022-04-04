fabfe4e5fa92db0f27f7725923f635634fdae091
WalterBanda <56268243+WalterBanda@users.noreply.github.com>
2022-04-04 18:08:20 +0300

    🔐 Added more Icons , Final touches on Auth Pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,72 +1,37 @@
-import { AuthPage, Button, Input, RegisterUser } from "../../components";
-import { auth } from "../../styles";
+import { AuthPage, RegisterUser } from "../../components";
+import { auth, sections } from "../../styles";
+import { useRouter } from "next/router";
+
+function Authlets() {
+  const router = useRouter();
+
+  const login_nav = () => router.push("/auth/login");
+
+  return (
+    <div className={sections.authlets}>
+      <Authlet icon="verifier-google" />
+      <Authlet icon="verifier-github" />
+      <Authlet
+        icon="verifier-email"
+        className={sections.email}
+        callback={login_nav}
+      />
+    </div>
+  );
+}
+
+function Authlet({ icon, callback, className }) {
+  return (
+    <button className={`${className} ${sections.authlet}`} onClick={callback}>
+      <i className={icon ?? "verifier-info"} />
+    </button>
+  );
+}
 export default function Index() {
   return (
     <div className={auth.onboarding}>
       <p>Login to your Account</p>
-      <>
-        <div
-          style={{
-            display: "inline-flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              width: 68,
-              height: 66,
-              paddingTop: 15,
-              paddingBottom: 15,
-              paddingLeft: 16,
-              paddingRight: 17,
-              backgroundColor: "black",
-              borderRadius: 9,
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <img
-              style={{ width: 35, height: 36, borderRadius: 8 }}
-              src="https://via.placeholder.com/35x36"
-            />
-          </div>
-          <div style={{ width: 20 }} />
-          <div
-            style={{
-              width: 68,
-              height: 66,
-              paddingTop: 16,
-              paddingBottom: 14,
-              paddingLeft: 18,
-              paddingRight: 14,
-              backgroundColor: "black",
-              borderRadius: 9,
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              style={{
-                width: 36,
-                height: 36,
-                backgroundColor: "rgba(66, 133, 244, 1)",
-                borderRadius: 8,
-              }}
-            />
-          </div>
-          <div style={{ width: 20 }} />
-          <img
-            style={{ width: 68, height: 66, borderRadius: 8 }}
-            src="https://via.placeholder.com/68x66"
-          />
-        </div>
-      </>
+      <Authlets />
       <RegisterUser />
     </div>
   );
