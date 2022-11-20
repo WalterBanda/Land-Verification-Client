bea55dcdd6f60d45c9d78cb1ac8f1b547e3f9c7a
Walter Banda <walterkaimurima@gmail.com>
2022-11-20 17:21:10 +0300

    ☕Refactored dashboard routes

--- components/core/sidebar.jsx
+++ components/core/sidebar.jsx
@@ -1,40 +1,58 @@
 import Link from "next/link";
-import {useRouter} from "next/router";
-import {sidebar} from "../../styles";
-import {useEffect, useState} from "react";
+import { useRouter } from "next/router";
+import { sidebar } from "../../styles";
+import { useEffect, useState } from "react";
 
 /**
  *
  * @param {JSX} children - This `replaces the Sidebar children ` used to set custom widgets for Sidebar
  * @returns
  */
-export default function Sidebar({children}) {
-    return (
-        children ?? (
-            <div className={sidebar.root}>
-                <SidebarItem itemName="dashboard" itemIcon="verifier-home" pageLink="/home"/>
-                <SidebarItem itemName="land owners" itemIcon="verifier-history" pageLink="/owners"/>
-                <SidebarItem itemName="stats" itemIcon="verifier-transactions" pageLink="/transactions"/>
-                <SidebarItem itemName="settings" itemIcon="verifier-settings" pageLink="/settings"/>
-            </div>
-        )
-    );
+export default function Sidebar({ children }) {
+  return (
+    children ?? (
+      <div className={sidebar.root}>
+        <SidebarItem
+          itemName="dashboard"
+          itemIcon="verifier-home"
+          pageLink="/dashboard"
+        />
+        <SidebarItem
+          itemName="land owners"
+          itemIcon="verifier-history"
+          pageLink="/dashboard/owners"
+        />
+        <SidebarItem
+          itemName="stats"
+          itemIcon="verifier-transactions"
+          pageLink="/dashboard/transactions"
+        />
+        <SidebarItem
+          itemName="settings"
+          itemIcon="verifier-settings"
+          pageLink="/dashboard/settings"
+        />
+      </div>
+    )
+  );
 }
 
-function SidebarItem({itemName, itemIcon, pageLink}) {
-    const router = useRouter();
-    const [active, setActive] = useState({});
+function SidebarItem({ itemName, itemIcon, pageLink }) {
+  const router = useRouter();
+  const [active, setActive] = useState({});
 
-    useEffect(() => {
-        setActive({color: router.pathname.startsWith(pageLink) ? "var(--text-active)" : "",})
-    }, [pageLink, router.pathname])
+  useEffect(() => {
+    setActive({
+      color: router.pathname.startsWith(pageLink) ? "var(--text-active)" : "",
+    });
+  }, [pageLink, router.pathname]);
 
-    return (
-        <Link href={pageLink} passHref>
-            <div className={`${sidebar.item}`} style={active}>
-                <i className={`${itemIcon}`}/>
-                <h2>{itemName}</h2>
-            </div>
-        </Link>
-    );
+  return (
+    <Link href={pageLink} passHref>
+      <div className={`${sidebar.item}`} style={active}>
+        <i className={`${itemIcon}`} />
+        <h2>{itemName}</h2>
+      </div>
+    </Link>
+  );
 }
