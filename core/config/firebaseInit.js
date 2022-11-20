accd29af56ba6b6db4e55de44f90ae1741bf3503
Walter Banda <walterkaimurima@gmail.com>
2022-11-20 14:35:39 +0300

    ☕Updated firebase init

--- core/config/firebaseInit.js
+++ core/config/firebaseInit.js
@@ -1,10 +1,10 @@
-import {connectFirestoreEmulator, getFirestore} from "firebase/firestore";
-import {getApp,getApps,initializeApp} from "firebase/app";
-import {connectFunctionsEmulator, getFunctions} from "firebase/functions";
-import {connectStorageEmulator, getStorage} from "firebase/storage";
-import {app_prod_mode, firebaseConfig} from "./firebase.config";
-import {getAuth} from "firebase/auth";
-import {getAnalytics} from "firebase/analytics"
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
+import { app_prod_mode, firebaseConfig } from "./firebase.config";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics"
 
 
 //? Manual Initialization
@@ -24,24 +24,14 @@ const firebaseAuth = getAuth(app);
 
 //? Init Emulators
 if (app_prod_mode) {
-    // connectAuthEmulator(firebaseAuth, "http://localhost:9099");
+    connectAuthEmulator(firebaseAuth, "http://localhost:9099");
     connectFirestoreEmulator(firestoreDB, "localhost", 8080);
     connectFunctionsEmulator(getFunctions(app), "localhost", 5001);
     connectStorageEmulator(storage, "localhost", 9199);
 }
 
 
-function initEmulators() {
-
-    if (app_prod_mode) {
-        connectFirestoreEmulator(getFirestore(getApp()), "localhost", 8080);
-        connectFunctionsEmulator(getFunctions(getApp()), "localhost", 5001);
-        connectStorageEmulator(getStorage(getApp()), "localhost", 9199);
-    }
-}
-
 export {
-    initEmulators,
     firebaseAuth,
     app,
     firestoreDB,
