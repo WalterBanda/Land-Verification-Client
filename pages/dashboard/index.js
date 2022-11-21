c7450665e2bfc854d43b0bf22b2196e6d6e6f537
Walter Banda <walterkaimurima@gmail.com>
2022-11-21 14:24:29 +0300

    ☕ Added Initial manage options

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,16 +3,34 @@ import {
     AccountDetails,
     RecentTransaction, PageBuilder
 } from "@components/index";
-import { home } from "@styles/index";
+import { home, components, sections } from "@styles/index";
+
+function AddLand() {
+    return <div className={components.stats}>
+        <h2>Add Land</h2>
+    </div>
+}
+
+function AddTransaction() {
+    return <div className={components.stats}>
+        <h2>Add Transaction</h2>
+    </div>
+}
+
 
 export default function Index() {
 
     return (
         <div className={home.root}>
-            <TransactionStats />
-            <AccountDetails />
+            <div className={sections.root}>
+                <p className={sections.header}>Manage</p>
+                <div className={sections.stats}>
+                    <AddLand />
+                    <AddTransaction />
+                </div>
+            </div>
             <RecentTransaction />
-        </div>
+        </div >
     );
 }
 Index.getLayout = function getLayout(page) {
